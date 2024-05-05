125c5f8c2a16d3c346ba7f217446156ded23e2c9
shaked <shaked4041@gmail.com>
2024-05-05 15:29:46 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import ChoosePlayer from './pages/ChoosePlayer'
 
 
 export default function App() {
   return (
     <div>
+      <ChoosePlayer/>
     </div>
   )
 }
--- src/pages/ChoosePlayer/index.jsx
+++ src/pages/ChoosePlayer/index.jsx
@@ -8,24 +8,32 @@ import BackCard from '../../components/BackCard'
 
 
 export default function ChoosePlayer() {
+
+  const handleChooose = () => {
+    alert('hi')
+  }
   return (
     <div className={styles.container}>
       <div className={styles.backButton}>
         <BackButton />
       </div>
       <div className={styles.headlineWrapper}>
-        <div className={styles.elipse} /> 
+        <div className={styles.elipse} />
         <span className={styles.text}>Choose Player</span>
       </div>
 
       <div className={styles.table}>
         <Wrrapper>
-          <BackCard height="150px" width="150px">
-            <ShapeX height="80px" width="80px" />
-          </BackCard>
-          <BackCard height="150px" width="150px">
-            <ShapeO height="80px" width="80px" />
-          </BackCard>
+          <div onClick={handleChooose()}>
+            <BackCard height="150px" width="150px" >
+              <ShapeX height="80px" width="80px" />
+            </BackCard>
+          </div>
+          <div onClick={handleChooose}>
+            <BackCard height="150px" width="150px">
+              <ShapeO height="80px" width="80px" />
+            </BackCard>
+          </div>
         </Wrrapper>
       </div>
     </div>
