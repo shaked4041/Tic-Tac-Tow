8e2130ec66115b39fb03877ccae05793f5e75fb2
shaked <shaked4041@gmail.com>
2024-05-06 11:28:02 +0300

    commit

--- src/components/BackCard/index.jsx
+++ src/components/BackCard/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from './style.module.scss'
 
 
-export default function BackCard({ children, height, width }) {
+export default function BackCard({ children, height, width, color }) {
   return (
-    <div className={styles.container} style={{ height: height, width: width }}>
+    <div className={styles.container} style={{ height, width, backgroundColor: color }}>
       {children}
     </div>
   )
--- src/pages/ChoosePlayer/index.jsx
+++ src/pages/ChoosePlayer/index.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import BackButton from '../../components/BackButton'
-import styles from './style.module.scss'
-import Wrrapper from '../../components/Wrrapper'
-import ShapeX from '../../components/ShapeX'
-import ShapeO from '../../components/ShapeO'
-import BackCard from '../../components/BackCard'
-
+import React, { useState } from 'react';
+import BackButton from '../../components/BackButton';
+import styles from './style.module.scss';
+import Wrapper from '../../components/Wrapper';
+import ShapeX from '../../components/ShapeX';
+import ShapeO from '../../components/ShapeO';
+import BackCard from '../../components/BackCard';
 
 export default function ChoosePlayer() {
+  const [x, setX] = useState(0);
+  const [o, setO] = useState(0);
 
-  const handleChooose = () => {
-    alert('hi')
+  const handleX = () => {
+    if (x == 1) {
+      setX(0)
+      setO(0)
+    } else {
+      setX(1)
+      setO(-1)
+    }
   }
+  const handleO = () => {
+    if (o == 1) {
+      setX(0)
+      setO(0)
+    } else {
+      setX(-1)
+      setO(1)
+    }
+  }
+
+  let stX = x == 0 ? { height: "80px", width: "80px" } : x == 1 ? { height: "160px", width: "160px" } : { height: "80px", width: "80px", fill: '#9B9B9B' }
+  let stO = o == 0 ? { height: "80px", width: "80px" } : o == 1 ? { height: "160px", width: "160px" } : { height: "80px", width: "80px", fill: '#9B9B9B' }
   return (
     <div className={styles.container}>
       <div className={styles.backButton}>
@@ -23,19 +42,23 @@ export default function ChoosePlayer() {
       </div>
 
       <div className={styles.table}>
-        <Wrrapper>
-          <div onClick={handleChooose()}>
-            <BackCard height="150px" width="150px" >
-              <ShapeX height="80px" width="80px" />
+        <Wrapper>
+          <div onClick={handleX} id="x">
+            <BackCard height="150px" width="150px" color={x == -1 ? 'grey' : ''}>
+              <div>
+                <ShapeX {...stX} />
+              </div>
             </BackCard>
           </div>
-          <div onClick={handleChooose}>
-            <BackCard height="150px" width="150px">
-              <ShapeO height="80px" width="80px" />
+          <div onClick={handleO} id="o">
+            <BackCard height="150px" width="150px" color={o == -1 ? 'grey' : ''}>
+              <div>
+                <ShapeO {...stO} />
+              </div>
             </BackCard>
           </div>
-        </Wrrapper>
-      </div>
-    </div>
-  )
+        </Wrapper>
+      </div >
+    </div >
+  );
 }
