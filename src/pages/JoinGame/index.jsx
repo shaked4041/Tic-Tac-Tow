772a92a85bdcdddf5756b5d7d5bd45c86775befc
liel_ben_him <lielcohen213@gmail.com>
2024-05-07 14:45:36 +0300

    commit

--- src/pages/JoinGame/index.jsx
+++ src/pages/JoinGame/index.jsx
@@ -5,8 +5,8 @@ import Wrapper from '../../components/Wrapper'
 import MainButton from '../../components/MainButton'
 
 const text1 = "join"
-const height1 = '70px'
-const width1 = '200px'
+const height1 = '60px'
+const width1 = '160px'
 
 const text2 = "create a game"
 const height2 = '70px'
@@ -23,9 +23,13 @@ export default function index() {
                     <span className={styles.titel}>join to a game</span>
                 </div>
 
-                <Wrapper><span className={styles.enterCode}>enter code game</span></Wrapper>
-                
-                <MainButton text={text1} height={height1} width={width1}></MainButton>
+                <div className={styles.enterCode}>
+                    <Wrapper><span className={styles.enterCode}>enter code game</span></Wrapper>
+                </div>
+
+                <div className={styles.btn1}>
+                    <MainButton className={styles.btn1} text={text1} height={height1} width={width1}></MainButton>
+                </div>
 
                 <div className={styles.or}>
                     <div className={styles.line}></div>
@@ -33,8 +37,11 @@ export default function index() {
                     <div className={styles.line}></div>
                 </div>
 
-                <MainButton text={text2} height={height2} width={width2}></MainButton>
+                <div className={styles.btn1}>
+                    <MainButton  text={text2} height={height2} width={width2}></MainButton>
+                </div>
             </div>
+
         </div>
     )
 }
