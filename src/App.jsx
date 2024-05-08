5565a3b44a549483d64fb780bea4824302b9bac9
liel_ben_him <lielcohen213@gmail.com>
2024-05-08 13:12:40 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import './App.css'
 import JoinGame from './pages/JoinGame'
 import ChoosePlayer from './pages/ChoosePlayer'
 import BoardPlayers from './pages/BoardPlayers/index'
-import ChoosePlayer from './pages/ChoosePlayer/index'
-import JoinGame from './pages/JoinGame/index'
-
 import Menu from './pages/Menu'
-import Waiting from './pages/Waiting'
+import WaitingJoin from './pages/WaitingJoin'
+import Waiting
+ from './pages/Waiting'
 
 
 export default function App() {
@@ -14,21 +13,14 @@ export default function App() {
     <div>
       {/* <JoinGame/> */}
 
-      {/* <ChoosePlayer/> */}
+      <ChoosePlayer/>
       {/* <Menu/> */}
+{/* <WaitingJoin/> */}
 {/* <Waiting/> */}
 {/* <ChoosePlayer/> */}
 
-<Routes>
-  <Route/>
-</Routes>
+
     </div>
   )
 
-  //   <div>
-  //     {/* <ChoosePlayer/> */}
-  //     {/* <Menu/> */}
-  //     <BoardPlayers />
-  //     {/* <JoinGame/> */}
-  //   </div>
 }
