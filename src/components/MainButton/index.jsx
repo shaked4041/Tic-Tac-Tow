bdff6269127669d4370cdfb0934ef9c9d3377294
liel_ben_him <lielcohen213@gmail.com>
2024-05-08 11:10:21 +0300

    commit

--- src/components/MainButton/index.jsx
+++ src/components/MainButton/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './style.module.scss'
 
-export default function index({ text, height, width }) {
+export default function index({ text, height, width ,onClick}) {
   return (
 
-        <button className={`${styles.btn}`} style={{ height, width }} >
+        <button className={`${styles.btn}`} style={{ height, width }} onClick={onClick}>
        <span className={styles.text}>{text}</span>    
         </button>
   
