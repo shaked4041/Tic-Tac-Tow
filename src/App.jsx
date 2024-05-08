37c827b51c2b3a4c9c7d34327c2d4fcd9fb3e491
liel_ben_him <lielcohen213@gmail.com>
2024-05-08 11:05:45 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import JoinGame from './pages/JoinGame'
 import ChoosePlayer from './pages/ChoosePlayer'
 
 import Menu from './pages/Menu'
+import Waiting from './pages/Waiting'
 
 export default function App() {
   return (
     <div>
-      <JoinGame/>
+      {/* <JoinGame/> */}
 
       {/* <ChoosePlayer/>
       <Menu/> */}
-
+<Waiting/>
 
     </div>
   )
